cc07806d3f714a331fda0ea3955e6627c1560404
anmolsaxena10 <anmol.saxena10@gmail.com>
2019-02-17 10:52:17 +0530

    models added

--- wineApp/models.py
+++ wineApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+#,country,description,designation,points,price,province,region_1,region_2,variety,winery
 # Create your models here.
+class Wine(models.Model):
+    country = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    designation = models.CharField(max_length=200)
+    points = models.IntegerField()
+    price = models.FloatField()
+    province = models.CharField(max_length=200)
+    region_1 = models.CharField(max_length=200)
+    region_2 = models.CharField(max_length=200)
+    variety = models.CharField(max_length=200)
+    winery = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.designation+' '+self.price+' '+self.country
--- wineApp/urls.py
+++ wineApp/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/wine', include('wine.urls'))
+    path('', home)
 ]
