7efaeef08dd349d38e656a99b46420e5b911ab0e
anmolsaxena10 <anmol.saxena10@gmail.com>
2019-02-18 18:00:28 +0530

    sorting and filtering completed

--- wineApp/views.py
+++ wineApp/views.py
@@ -22,18 +22,22 @@ class Filtered(generics.ListAPIView):
         province = self.request.query_params.get('province', None)
         region_1 = self.request.query_params.get('region_1', None)
         region_2 = self.request.query_params.get('region_2', None)
+        sort_order = self.request.query_params.get('sort_order', None)
         print(country)
 
         queryset = Wine.objects.all()
         if country is not None:
-            queryset = queryset.filter(country = country)
+            queryset = queryset.filter(country__icontains = country)
         if province is not None:
-            queryset = queryset.filter(province = province)
+            queryset = queryset.filter(province__icontains = province)
         if region_1 is not None:
-            queryset = queryset.filter(region_1 = region_1)
+            queryset = queryset.filter(region_1__icontains = region_1)
         if region_2 is not None:
-            queryset = queryset.filter(region_1 = region_2)
-
+            queryset = queryset.filter(region_1__icontains = region_2)
+        if sort_order == 'ASC':
+            queryset = queryset.order_by('price')
+        if sort_order == 'DESC':
+            queryset = queryset.order_by('price').reverse()
         return queryset
 
 
